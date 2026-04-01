9042f9d2cd8fa3c3a797e0cc14b3c8ec9c1e385e
Hristo Petkov <petkov.t.hristo@gmail.com>
2026-04-01 23:40:27 +0300

    test: Added test cases for subtraction function.
    Introduced new test cases to verify subtraction of:
    
    - Integers
    - Floats
    - Boolean values

--- test/test_my_math.py
+++ test/test_my_math.py
@@ -52,7 +52,33 @@ class TestAddition(unittest.TestCase):
         result = test_object.addition(True, True)
         assert result == 2, "Expected 2, received {0}.".format(result)
 
+class TestSubtraction(unittest.TestCase):
+    '''
+    Unit test class for validating the behavior of subtraction function
+    '''
+    def test_subtracting_integers(self):
+        '''
+        Verify the subtraction function returns 10 when provided with 2 valid positive integers (a = 15, b = 5).
+        '''
+        test_object = MyMath()
+        result = test_object.subtraction(15,5)
+        assert result == 10, "Expected 10, received {0}.".format(result)
 
+    def test_subtracting_floats(self):
+        '''
+        Verify the subtraction function returns -2.6 when provided with 2 valid positive floats (a = 6.3, b = 8.9).
+        '''
+        test_object = MyMath()
+        result = round(test_object.subtraction(6.3, 8.9), 1)
+        assert result == -2.6, "Expected -2.6, received {0}.".format(result)
+    
+    def test_subtracting_boolean(self):
+        '''
+        Verify the subtraction function returns 0 when provided with 2 valid booleans (a = True, b = True).
+        '''
+        test_object = MyMath()
+        result = test_object.subtraction(True, True)
+        assert result == 0, "Expected 0, received {0}.".format(result)
 
 if __name__ == '__main__':
     unittest.main()
