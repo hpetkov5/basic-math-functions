d865722f49f34291eba4a3197098eb1604fd2cf6
Hristo Petkov <petkov.t.hristo@gmail.com>
2026-04-05 22:50:38 +0300

    refactor: Updated my_math.py
    Updated the functions in my_math.py
    Updated docstrings in my_math.py

--- src/my_math.py
+++ src/my_math.py
@@ -1,39 +1,51 @@
 '''
-MyMath class contains 4 functions (addition, subtraction, multiplication, division).
+This module contains the MyMath class, which provides basic mathematical operations.
 '''
+
+
 class MyMath:
     '''
-    
+    This class contains 4 functions (addition, subtraction, multiplication, division).
     '''
-    def addition(self,a,b):
-        '''
-        This function takes 2 objects and adds them together.
-        Works with: Integers, Floats, Strings, Lists, Booleans
 
+    @staticmethod
+    def addition(num1: int, num2: int) -> int:
         '''
-        return a+b
-    
-    def subtraction(self,a,b):
+        This function takes 2 integers and adds them together.
+        :param num1: The first integer to be added.
+        :param num2: The second integer to be added.
+        :return: The sum of num1 and num2.
         '''
-        This function takes the value from object b and subtracts it from the value of object a.
-        Works with: Integers, Floats, Booleans
+        return num1 + num2
 
+    @staticmethod
+    def subtraction(num1: int, num2: int) -> int:
         '''
-        return a-b
-    
-    def multiplication(self,a,b):
+        This function takes the 2 integers and subtracts num2 from num1.
+        :param num1: The first integer to be subtracted from.
+        :param num2: The second integer to be subtracted.
+        :return: The difference of num1 and num2.
         '''
-        This function multiplies <a> times <b>.
-        Works with: Integers, Floats, Booleans, Strings (repeats string <a> <b> times), Lists (repeats list <a> <b> times), Tuples (repeats tuple <a> <b> times)
+        return num1 - num2
 
+    @staticmethod
+    def multiplication(num1: int, num2: int) -> int:
         '''
-        return a*b
-    
-    def division(self,a,b):
+        This function multiplies the 2 integers.
+        :param num1: The first integer to be multiplied.
+        :param num2: The second integer to be multiplied.
+        :return: The product of num1 and num2.
         '''
-        This function divides object <a> into <b> equal parts.
-        Works with: Integers, Floats, Booleans
+        return num1 * num2
 
+    @staticmethod
+    def division(num1: int, num2: int) -> float:
         '''
-        return a/b
-    
+        This function divides num1 by num2.
+        :param num1: The first integer to be divided.
+        :param num2: The second integer to be divided by.
+        :return: The quotient of num1 and num2.
+        '''
+        if num2 == 0:
+            raise ValueError("Cannot divide by zero.")
+        return num1 / num2
